d8c6455aaf3fbff586437906288126fe73869e10
17robots <mdray@ameritech.net>
2020-12-13 15:49:58 -0500

    Add 12-3

--- 2020/12-2/12-2.cpp
+++ 2020/12-2/12-2.cpp
@@ -7,6 +7,13 @@
 
 int countChar(std::string password, char desiredChar)
 {
+  int count = 0;
+  for (auto item : password)
+  {
+    if (item == desiredChar)
+      count++;
+  }
+  return count;
 }
 
 int main()
@@ -15,10 +22,18 @@ int main()
   is.open("input.txt", std::ios::in);
   std::string line, range, letter, password;
   std::stringstream ss;
+  int validPasswords = 0;
   while (std::getline(is, line, '\n'))
   {
-    ss >> line;
-    ss << range << letter << password;
+    ss << line;
+    ss >> range >> letter >> password;
+
+    char desiredChar = letter.at(0);
+    int min = std::stoi(range.substr(0, range.find("-")));
+    int max = std::stoi(range.substr(range.find("-") + 1));
+    if ((password.at(min - 1) == desiredChar && password.at(max - 1) != desiredChar) || (password.at(min - 1) != desiredChar && password.at(max - 1) == desiredChar))
+      validPasswords++;
   }
+  std::cout << "Valid Passwords: " << validPasswords;
   return 0;
 }
